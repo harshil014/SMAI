d12fff9746e101b773f3bd57e9521fc8645f6b13
Kushagra Chandak <kushagrachandak06@gmail.com>
2016-09-23 18:10:42 +0530

    None to -1

--- load.py
+++ load.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 import csv
 def loadCsv(filename):
-	lines = csv.reader(open(filename, "rb"))
-	dataset = list(lines)
-	print dataset[1]
-	for i in range(len(dataset)):
-		for j in range(len(dataset[i])):
-			if dataset[i][j] == 'NULL':
-				dataset[i][j] = float(-1)
-			else:
-				dataset[i][j] = float(dataset[i][j])
-	#	dataset[i] = [float(x) for x in dataset[i] if x not in ['NULL']]
-	return dataset
+    lines = csv.reader(open(filename, "rb"))
+    dataset = list(lines)
+#    print dataset[1]
+    csvfile = open('1.csv','w') 
+    write = csv.writer(csvfile, delimiter=' ')
+    for i in range(len(dataset)):
+        for j in range(len(dataset[i])):
+            if dataset[i][j] == 'NULL':
+                dataset[i][j] = -1
+        write.writerow(dataset[i])
+    with open(filename) as f:
+            z = csv.reader(f, delimiter='\t')
+    return dataset
 
-
-filename = 'set.csv'
+filename = 'train19.csv'
 dataset = loadCsv(filename)
 print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
-print dataset[1]
+#print dataset[1]
+
