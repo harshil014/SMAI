9fb64e226c5288f3389602bea78c9900368b86a5
harshil014 <harshil.jain@students.iiit.ac.in>
2016-09-28 19:59:33 +0530

    Till_Prior

--- date.py
+++ date.py
@@ -3,10 +3,18 @@ from datetime import date
 def diff_dates( date1, date2):
 	return abs(date2-date1).days
 
+def func():
+	x=input()
+	print x
+
+
 def main():
+	
     d1 = date(2013,1,1)
-    d2 = date(2013,9,13)
+    d2 = date(2016,1,1)
     result1 = diff_dates(d2, d1)
     print '{}'.format(result1)
+    
+func()
 
 main()
---
+++ new.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import csv
+import math
+
+
+def loadCsv(filename):
+    lines = csv.reader(open(filename, 'rb'))
+    dataset = list(lines)
+
+#    print dataset[1]
+
+    count = 0
+    csvfile = open('2.csv', 'w')
+    write = csv.writer(csvfile, delimiter=',')
+    for i in range(len(dataset)):
+        flag = 0
+        for j in range(len(dataset[i])):
+            if dataset[i][j] == '':
+                dataset[i][j] = 0
+            else:
+                dataset[i][j] = int(math.floor(float(dataset[i][j])))
+    write.writerow(dataset[i])
+
+#    print count
+
+    with open(filename) as f:
+        z = csv.reader(f, delimiter=',')
+    return dataset
+
+
+def separateByCluster(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if vector[-1] not in separated:
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+def calculate_prior(cluster_data,k):
+    temp=[]
+    for i in range(len(cluster_data)):
+        print len(cluster_data[i]),
+        f=float(len(cluster_data[0]))
+        temp.append(f/k)
+    return temp
+
+def main():
+    filename = 'ab.csv'
+    dataset = loadCsv(filename)
+    print 'Loaded data file {0} with {1} rows'.format(filename,len(dataset))
+    print 'Data Clustering '
+    cluster_data = separateByCluster(dataset)
+    print len(cluster_data)
+    prior =  calculate_prior(cluster_data,len(dataset))
+    print prior[0]
+
+main()
+
+
+			
