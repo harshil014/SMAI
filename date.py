ba1d06b6915cbb4984c340990982716137ad097b
harshil014 <harshil.jain@students.iiit.ac.in>
2016-09-28 10:15:33 +0530

    Count days added

---
+++ date.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+def diff_dates( date1, date2):
+	return abs(date2-date1).days
+
+def main():
+    d1 = date(2013,1,1)
+    d2 = date(2013,9,13)
+    result1 = diff_dates(d2, d1)
+    print '{}'.format(result1)
+
+main()
